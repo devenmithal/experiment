6ea666de1e7db10171e20791281e8f6059d41b8c
devenmithal <devenmithal@gmail.com>
2018-02-09 23:50:49 -0500

    Experimenting from the android studio.

--- .idea/firstexperiment.java
+++ .idea/firstexperiment.java
@@ -1,7 +1,23 @@
 public class Experiment{
 
-    public static void main(String[] args) {
-        System.out.println("First experiment");
-    }
+        public static void main(String[] argv)
+        {
+            int tal = 0;
+            int summa = 0;
+
+            for(tal = 1; tal < 1000; tal++)
+            {
+                if(tal%3 == 0)
+                {
+                    summa += tal;
+                    continue;
+                }
+                else if (tal%5 == 0)
+                {
+                    summa += tal;
+                }
+            }
+            System.out.println(summa);
+        }
 
 }
